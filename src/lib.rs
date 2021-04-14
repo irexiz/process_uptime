366b9c6555208a577a92e7148d5060934fcbe8d3
iz <iz@genesismobo.com>
2021-04-14 14:17:51 +0200

    get uptime via ps etime command

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use anyhow::{anyhow, Context, Result};
 use std::convert::TryFrom;
-use std::error;
 use std::fs;
 use std::process;
 use std::time;
+
+use process::Command;
 
 pub struct ProcessUptime {
     pub pid: u32,
@@ -10,17 +12,44 @@ pub struct ProcessUptime {
 }
 
 impl ProcessUptime {
-    pub fn new() -> Result<Self, Box<dyn error::Error>> {
+    pub fn new() -> Result<Self> {
         let pid = process::id();
-        let metadata = fs::metadata(format!("/proc/{}", pid))?;
-        let uptime = metadata.modified()?.elapsed()?;
+
+        let uptime = match Self::get_ps_etime(pid) {
+            Ok(etime) => time::Duration::from_secs(etime),
+            Err(_) => {
+                // Fallback to getting from /proc/{}
+                let metadata = fs::metadata(format!("/proc/{}", pid))?;
+                metadata.modified()?.elapsed()?
+            }
+        };
 
         Ok(Self { pid, uptime })
+    }
+
+    fn get_ps_etime(pid: u32) -> Result<u64> {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(format!("ps -o etimes -p {} --no-headers", pid).as_str())
+            .output()?;
+
+        let mut uptime_string = std::str::from_utf8(output.stdout.as_slice())?.to_string();
+
+        if uptime_string.ends_with('\n') {
+            uptime_string.pop();
+            if uptime_string.ends_with('\r') {
+                uptime_string.pop();
+            }
+        }
+
+        uptime_string
+            .parse::<u64>()
+            .with_context(|| anyhow!("Failed to parse {} to u64", uptime_string))
     }
 }
 
 impl TryFrom<u32> for ProcessUptime {
-    type Error = Box<dyn error::Error>;
+    type Error = anyhow::Error;
     fn try_from(pid: u32) -> Result<Self, Self::Error> {
         let metadata = fs::metadata(format!("/proc/{}", pid))?;
         let uptime = metadata.modified()?.elapsed()?;
