8ce4f2110f71fd2c1f46d987ad107532cc5cfbad
iz <iz@genesismobo.com>
2021-12-20 08:48:31 +0100

    Use /proc/pid/stat starttime to calculate actual process uptime

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+use anyhow::bail;
 use anyhow::{anyhow, Context, Result};
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
 use std::fs;
 use std::process;
-use std::time;
+use std::time::{self, Duration};
 
 use process::Command;
 
@@ -11,6 +12,8 @@ pub struct ProcessUptime {
     pub uptime: time::Duration,
 }
 
+const PROC_UPTIME: &str = "/proc/uptime";
+
 impl ProcessUptime {
     pub fn new() -> Result<Self> {
         let pid = process::id();
@@ -18,13 +21,45 @@ impl ProcessUptime {
         let uptime = match Self::get_ps_etime(pid) {
             Ok(etime) => time::Duration::from_secs(etime),
             Err(_) => {
-                // Fallback to getting from /proc/{}
-                let metadata = fs::metadata(format!("/proc/{}", pid))?;
-                metadata.modified()?.elapsed()?
+                // Fallback to getting from /proc/{pid}/stat
+                Self::proc_stat_uptime(pid)?
             }
         };
 
         Ok(Self { pid, uptime })
+    }
+
+    pub(crate) fn proc_stat_uptime(pid: u32) -> Result<Duration> {
+        let content = fs::read_to_string(PROC_UPTIME)?;
+
+        let system_uptime = Self::system_uptime(&content)?;
+        let process_uptime = Self::process_uptime(pid)?;
+        let sc_clk_tck: u64 = unsafe { libc::sysconf(libc::_SC_CLK_TCK).try_into()? };
+
+        let process_uptime = Duration::from_secs(system_uptime - process_uptime / sc_clk_tck);
+        Ok(process_uptime)
+    }
+
+    fn process_uptime(pid: u32) -> Result<u64> {
+        let content = fs::read_to_string(format!("/proc/{}/stat", pid))?;
+        let process_uptime = content
+            .split_whitespace()
+            .nth(21)
+            .ok_or_else(|| anyhow!("/proc/{}/stat 22nd column not found", pid))?;
+        let process_uptime = process_uptime.parse::<f32>()? as u64;
+
+        Ok(process_uptime)
+    }
+
+    pub(crate) fn system_uptime(content: &str) -> Result<u64> {
+        let uptime = content.split_whitespace().next();
+
+        let uptime = match uptime {
+            Some(uptime) => uptime,
+            None => bail!("`/proc/uptime` 0th field not present"),
+        };
+
+        Ok(uptime.parse::<f32>()? as u64)
     }
 
     fn get_ps_etime(pid: u32) -> Result<u64> {
@@ -55,5 +90,31 @@ impl TryFrom<u32> for ProcessUptime {
         let uptime = metadata.modified()?.elapsed()?;
 
         Ok(Self { pid, uptime })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{process, time::Duration};
+
+    use crate::ProcessUptime;
+
+    #[test]
+    fn test_system_uptime() {
+        assert_eq!(
+            ProcessUptime::system_uptime("609773.79 19153047.14\n").unwrap(),
+            609773
+        )
+    }
+
+    #[ignore]
+    #[test]
+    fn test_this_process_uptime() {
+        std::thread::sleep(Duration::from_secs(3));
+        let pid = process::id();
+        assert_eq!(
+            ProcessUptime::proc_stat_uptime(pid).unwrap(),
+            Duration::from_secs(3)
+        );
     }
 }
